2fd41afbfa3c36f2ac999471fba44c6a6ea9482c
Sch <Sinchaohao@outlook.com>
2022-07-16 22:34:39 +0800

    移动alien群并转向

--- alien_invasion/game_functions.py
+++ alien_invasion/game_functions.py
@@ -69,8 +69,11 @@ def update_bullets(bullets):
     #     if bullet.rect.buttom <= 0:
     #         bullets.remove(bullet)
 
-def update_aliens(aliens):
-    aliens.update()
+def update_aliens(ai_settings, aliens):
+    """
+        检查是否有外星人位于屏幕边缘，并更新整群外星人的位置
+        """
+    check_fleet_edges(ai_settings, aliens)
 
 def fire_bullets(ai_settings, screen, ship, bullets):
     if len(bullets) < ai_settings.bullets_allowed:
@@ -113,6 +116,21 @@ def get_number_rows(ai_settings, ship_height, alien_height):
     available_space_y = ai_settings.screen_height - (5 * alien_height) - ship_height
     number_rows = int(available_space_y / (2 * alien_height))
     return number_rows
+
+def check_fleet_edges(ai_settings, aliens):
+    """有外星人到达边缘时采取相应的措施"""
+    for alien in aliens.sprites():
+        if alien.check_edges():
+            change_fleet_direction(ai_settings, aliens)
+            break
+
+def change_fleet_direction(ai_settings, aliens):
+    """将整群外星人下移，并改变它们的方向"""
+    for alien in aliens.sprites():
+        alien.rect.y += ai_settings.fleet_drop_speed
+    ai_settings.fleet_direction *= -1
+
+
 
 
 
