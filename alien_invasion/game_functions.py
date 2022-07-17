715f03090f07c1785564e194626ac87f13a490e9
Sch <Sinchaohao@outlook.com>
2022-07-17 14:26:54 +0800

    检测aline-ship碰撞

--- alien_invasion/game_functions.py
+++ alien_invasion/game_functions.py
@@ -110,6 +110,16 @@ def update_screen(ai_settings, screen, ship, bullets, aliens):
     # 让最近绘制的屏幕可见
     pygame.display.flip()
 
+
+def check_bullet_alien_collisions(ai_settings, screen, ship, aliens, bullets):
+    """响应子弹和外星人的碰撞"""
+    collisions = pygame.sprite.groupcollide(bullets, aliens, True, True)
+
+    if len(aliens) == 0:
+        # 删除现有的子弹并新建一群外星人
+        bullets.empty()
+        create_fleet(ai_settings, screen, ship, aliens)
+
 def update_bullets(ai_settings, screen, ship, aliens, bullets):
     """更新子弹的位置，并删除已消失的子弹"""
     # 更新子弹的位置
@@ -122,16 +132,16 @@ def update_bullets(ai_settings, screen, ship, aliens, bullets):
 
     # 检查是否有子弹击中了外星人
     # 如果是这样，就删除相应的子弹和外星人
-    collisions = pygame.sprite.groupcollide(bullets, aliens, True, True)
+    check_bullet_alien_collisions(ai_settings, screen, ship, aliens, bullets)
 
-    if len(aliens) == 0:
-        # 删除现有的子弹并新建一群外星人
-        bullets.empty()
-        create_fleet(ai_settings, screen, ship, aliens)
-
-def update_aliens(ai_settings, aliens):
+def update_aliens(ai_settings, ship, aliens):
     """
         检查是否有外星人位于屏幕边缘，并更新整群外星人的位置
         """
     check_fleet_edges(ai_settings, aliens)
+    aliens.update()
+
+    # 检测外星人和飞船之间的碰撞
+    if pygame.sprite.spritecollideany(ship, aliens):
+        print("Ship hit!!")
 
