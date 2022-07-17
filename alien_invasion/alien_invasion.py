008f4a5254381ccc65a64a311b99550972298806
Sch <Sinchaohao@outlook.com>
2022-07-17 15:49:34 +0800

    游戏信息统计

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -10,6 +10,7 @@ from settings import Settings
 from ship import Ship
 import game_functions as gf
 from alien import Alien
+from game_stats import GameStats
 
 def run_game():
     # 初始化游戏并创建一个屏幕对象
@@ -17,6 +18,8 @@ def run_game():
     ai_settings = Settings()
     screen = pygame.display.set_mode( (ai_settings.screen_width, ai_settings.screen_height) )
     pygame.display.set_caption("Alien Invasion")
+    # 创建一个用于存储游戏统计信息的实例
+    stats = GameStats(ai_settings)
 
     # 创建一艘飞船
     ship = Ship(screen, ai_settings)
